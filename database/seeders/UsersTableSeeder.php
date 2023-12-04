2495994845c03a76b20a8399171951178fb39cbe
Aldar Bazarov <a.bazarov.sas@gmail.com>
2023-12-04 19:26:13 +0500

    Авторизация

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // Создание обычных пользователей
+        User::factory(10)->create();
+
+        // Создание пользователя с ролью "admin"
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
+    }
+}
